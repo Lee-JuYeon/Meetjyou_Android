b7c5b6ab76aabb3a30743c122e0983f663266824
cavss <cavss1118@gmail.com>
2025-05-25 15:42:51 +0900

    Chore: Viewpager, BottomNavigation 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    // bottom navigation
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
+
+    // tablayout
+    implementation("androidx.viewpager2:viewpager2:1.1.0")
+
 }
