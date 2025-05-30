a6095658f1ea7d3c2fab443ff7777fcce877ce77
cavss <cavss1118@gmail.com>
2025-05-30 23:27:26 +0900

    Feat : 라이스스타일 타입 클래스 추가

---
+++ app/src/main/java/com/doggetdrunk/meetjyou_android/types/LifeStyleType.kt
@@ -0,0 +1,13 @@
+package com.doggetdrunk.meetjyou_android.types
+
+enum class LifeStyleType {
+    TWEENTY,
+    MALE,
+    OUTDOOR,
+    FREE,
+    SPORTS,
+    SHOPPING,
+    NATURE,
+    SMOKING,
+    DRINKING
+}
