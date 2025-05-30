d3e4e4713a31b2b6d68bdcf52ae604b726cb8b5e
cavss <cavss1118@gmail.com>
2025-05-30 23:39:41 +0900

    Feat : RequestPartyModel 추가

---
+++ app/src/main/java/com/doggetdrunk/meetjyou_android/model/RequestPartyModel.kt
@@ -0,0 +1,10 @@
+package com.doggetdrunk.meetjyou_android.model
+
+data class RequestPartyModel(
+    var uid : String,
+    var requestUserUid : String,
+    var partyUid : String,
+    var requestDescription : String
+){
+
+}
