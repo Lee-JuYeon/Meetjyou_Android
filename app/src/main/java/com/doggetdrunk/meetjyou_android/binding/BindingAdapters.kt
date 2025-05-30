f66478a053c527339fe83b45b2c6e7d1a29a5af0
cavss <cavss1118@gmail.com>
2025-05-30 23:07:24 +0900

    Refactor: 서버 이미지 받아올떄 PlaceHolder, Error Iameg 대응

--- app/src/main/java/com/doggetdrunk/meetjyou_android/binding/BindingAdapters.kt
+++ app/src/main/java/com/doggetdrunk/meetjyou_android/binding/BindingAdapters.kt
@@ -3,16 +3,11 @@ package com.doggetdrunk.meetjyou_android.binding
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.doggetdrunk.meetjyou_android.R
 
 object BindingAdapters {
 
-    @BindingAdapter("android:src")
-    @JvmStatic
-    fun setImageResource(imageView: ImageView, resourceId: Int) {
-        if (resourceId != 0) {
-            imageView.setImageResource(resourceId)
-        }
-    }
 
     @BindingAdapter("imageRes")
     @JvmStatic
@@ -22,13 +17,20 @@ object BindingAdapters {
         }
     }
 
+
     @BindingAdapter("imageUrl")
     @JvmStatic
     fun loadImageFromUrl(imageView: ImageView, url: String?) {
         if (!url.isNullOrEmpty()) {
             Glide.with(imageView.context)
                 .load(url)
+                .placeholder(R.drawable.ic_launcher_background)
+                .error(R.drawable.ic_launcher_background)
+                .centerCrop()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .into(imageView)
+        } else {
+            imageView.setImageResource(R.drawable.ic_launcher_background)
         }
     }
 }
